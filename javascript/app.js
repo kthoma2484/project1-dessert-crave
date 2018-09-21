001f464b8e8396674743ae5c6cf644c380ce546b
kthoma2484 <35786891+kthoma2484@users.noreply.github.com>
2018-09-20 17:00:23 -0700

    justed background image fit

--- javascript/app.js
+++ javascript/app.js
@@ -76,24 +76,4 @@ $(function () {
         themeSong.pause();
     });
     
-    // ----------------------Kim's Section Ends/Anthony's Section Starts----------------------------------
-
-    /*
-        // --------------------------------------------------
-        let queryURL = "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo"
-
-        $.ajax({
-            url: "https://www.googleapis.com/youtube/v3/AIzaSyBqsfshUVe2dhIcYn_bKTXrpr9Pst1g3Vo",
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-        })
-    */
-
-
-    // ---------------------Anthony's Section Ends/ Ericka's Section Starts----------------------------------
-
-
-
-
 });
